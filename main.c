5bf1ca9cc4bd709a184e2d702d0cd1df64ff5a95
jcarlosadm <carlosmaster10@gmail.com>
2014-08-07 12:19:56 -0300

    documentando main

--- main.c
+++ main.c
@@ -3,29 +3,62 @@
  *
  */
 
+// bibliotecas básicas
 #include <stdio.h>
 #include <allegro5/allegro.h>
+// módulos do jogo
 #include "window.h"
 #include "ships.h"
 
+// inicialização principal
 int init();
+
+// desalocação de memória dos objetos de jogo
 void finish_game(Player_ship* player);
 
 int main()
 {
-    // variáveis locais
+    /*******************************
+     * variáveis locais
+     *******************************/
+
+    /*
+     * objetos de jogo
+     */
+
+    // jogador
     Player_ship *player;
+
+    /*******************************
+     * Inicialização
+     *******************************/
+
+    /*
+     * Inicializações do Allegro
+     */
 
     if(!init()){
         fprintf(stderr, "falha ao inicializar\n");
         exit(EXIT_FAILURE);
     }
 
+    /************************************
+     * Alocações de objetos do jogo
+     ************************************/
+
+    /*
+     * Alocação do player
+     */
+
     player = new_player_ship(6, 7,400,200,32,32);
     if(player == NULL){
         finish_game(player);
         exit(EXIT_FAILURE);
     }
+
+    /************************************
+     * Processo principal
+     ************************************/
 
     al_clear_to_color(al_map_rgb_f(0,255,255));
 
@@ -34,6 +67,10 @@ int main()
 
     al_flip_display();
     al_rest(10.0);
+
+    /************************************
+     * Finalização
+     ************************************/
 
     finish_game(player);
 
